079f58c6c82b281a0d4847b9579d4a1c66cc2ae5
tanqueta88 <oliverprim783@gmail.com>
2023-08-28 14:04:06 -0300

    Folders C1, C2 and C3 are added.

---
+++ C1/Program.cs
@@ -0,0 +1,18 @@
+﻿// See https://aka.ms/new-console-template for more information
+string ciudad;
+//Asignacion de variable
+ciudad = "Beraza";
+
+#region 
+int lluvia1 = 22;
+int lluvia2 = 10;
+int lluviaTotal;
+#endregion
+
+
+#region 
+lluviaTotal = lluvia1 + lluvia2;
+//Ejecucion de la variable
+Console.WriteLine(ciudad);
+//Console.ReadKey();esta sentencia permite que la consola se quede esperando a que se precione el enter
+#endregion
