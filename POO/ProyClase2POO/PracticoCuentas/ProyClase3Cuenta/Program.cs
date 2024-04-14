1688423e6d1795a099431b95c89cfe95fcdf9801
tanqueta88 <oliverprim783@gmail.com>
2024-04-14 05:14:41 -0300

    Folder POOc2 is added

---
+++ POO/ProyClase2POO/PracticoCuentas/ProyClase3Cuenta/Program.cs
@@ -0,0 +1,31 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Hello, World!");
+var cuenta1=new Cuenta(11111, "Franco Buenaventura");
+Console.WriteLine(cuenta1.Titular);
+cuenta1.Saldo=10000;
+System.Console.WriteLine(cuenta1.Saldo);
+
+
+public class Cuenta{
+    //CONSTRUCTOR 1 = CONSTRUCTOR POR DEFAULT
+    public Cuenta(){
+
+    }
+    //CONSTRUCTOR 2 = CONSTRUCTOR CON PARAMETROS
+    public Cuenta(int nrocta, string nomtit){
+        Nro=nrocta;
+        Titular=nomtit;
+    }
+
+    public string Titular { get; set; }
+    public int Nro { get; set; }
+    public double Saldo { get; set; }
+    public void Depositar() { 
+
+     }
+
+     public void Extraer(){
+
+     }
+
+}
---
+++ POO/ProyClase2POO/ProyClase2POO/Program.cs
@@ -0,0 +1,87 @@
+﻿// ATRIBUTOS (Se van a pasar a la clase como Datos): Nombres, Apellido, fecha de nacimiento
+// COMPORTAMIENTOS (Se van a pasar a la clase como Metodos):
+
+var alumno1 = new Alumno();
+alumno1.Saludar();
+alumno1.Nombre = "Pepe";
+alumno1.Apellido = "Grillo";
+
+var alumno14=new Alumno(19391978);
+System.Console.WriteLine(alumno14.Documento);
+
+Alumno alumnoBecado = new Alumno();
+
+var alumnoLibre = new Alumno();
+
+alumnoLibre.Nombre= "Patuyo";
+alumnoBecado.Saludar();
+public class Alumno
+{
+    public Alumno(){
+
+    }
+
+        public Alumno(int pdni){
+            dni=pdni;
+        
+    }
+    // CAMPOS
+    string nombre;
+    public string Apellido{get;set;}
+    private int dni;
+    public int Documento
+    {
+        get { return dni; }
+        set { dni = value; }
+    }
+    //PROPIEDADES
+    public string Nombre
+    {
+        get { return nombre; }
+        set { nombre = value; }
+    }
+
+
+    // public string Apellido
+    // {
+    //     get { return apellido; }
+    //     set { apellido = value; }
+    // }
+
+    public string NombreCompleto
+    {
+        get { return nombre + " " + Apellido; }
+    }
+
+    public void Saludar()
+    {
+        nombre = "Laura";
+        System.Console.WriteLine("Hola soy: " + nombre);
+    }
+    private void Anotarse()
+    {
+
+    }
+}
+
+// private class Alumno
+// { //Todo lo que va adentro de la clase se llamand mienbros de la clase(pueden ser campos, propiedades, listas, metodos, otras clases)
+//   //Campos
+//     string nombre;
+//     string apellido;
+
+//     private int dni;
+//     //Metodos
+//     public void Saludar()
+//     {
+//         nombre = "Laura";
+//     }
+
+//     private void Anotarse()
+//     {
+
+//     }
+
+// }
+
+
